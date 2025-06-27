bea8126c6c028af72a5dcbcab6e1d1a9f389f152
Manokha Vadim <manokha.vadim@gmail.com>
2025-06-27 05:44:49 +0300

    modified mein.tsx

---
+++ src/components/App/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return <div>{/* App content goes here */}</div>;
+};
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./components/App/App";
+import App from "./components/App/App.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
